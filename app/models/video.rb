88573199574397f2b48f5cf896a781a29ff7181a
ruanvalente <ruanopensuselinux@gmail.com>
2023-03-26 21:55:13 -0300

    WIP(testing): create initial testing model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,8 @@
 class Video < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+  validates :category_id, presence: true
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class VideoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create video" do
+    video = Video.new :title => "Trainee 2022/2 - Models no Rails", :description => "Aula sobre models no Rails", :url => "https://www.youtube.com/watch?v=OQsCRI1hzec&list=PLPDMfYZSRJ0l5AhPMK_x12MsZPRLjY2KV&index=6", :category_id => 3
+
+    video.save
+
+    assert_equal "Trainee 2022/2 - Models no Rails", video.title
+    assert_equal "Aula sobre models no Rails", video.description
+    assert_equal "https://www.youtube.com/watch?v=OQsCRI1hzec&list=PLPDMfYZSRJ0l5AhPMK_x12MsZPRLjY2KV&index=6", video.url
+    assert_equal 3, video.category_id
+  end
+
+  test "should not created video without category id" do
+    video = Video.new :title => "Trainee 2022/2 - Models no Rails", :description => "Aula sobre models no Rails", :url => "https://www.youtube.com/watch?v=OQsCRI1hzec&list=PLPDMfYZSRJ0l5AhPMK_x12MsZPRLjY2KV&index=6", :category_id => nil
+
+    video.save
+    
+    assert_equal nil, video.category_id
+    assert_not false, video
+  end
 end
