ed5fb2f384970072d88dd2380710ef2112aef641
ruanvalente <ruanopensuselinux@gmail.com>
2023-03-28 23:02:18 -0300

    refactor(testing): refactor video testing

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Video, type: :model do
 
-  subject { Video.create(id: 1, title: "Basic rspec Ruby on Rails model specs", description: "In this episode you'll learn some basics for testing simple methods on Ruby on Rails models.", url: "https://www.youtube.com/watch?v=Spogv4o8haM", category_id: 3) }
-
-  let(:category) { Category.create(id: 3, title: "YouTube", color: "red") }
+  let!(:category) { Category.create(id: 3, title: "YouTube", color: "red") }
+  let!(:video) { Video.create(
+    id: 1, 
+    title: "Basic rspec Ruby on Rails model specs", 
+    description: "In this episode you'll learn some basics for testing simple methods on Ruby on Rails models.", 
+    url: "https://www.youtube.com/watch?v=Spogv4o8haM", 
+    category_id: category.id
+    ) }
   
   describe "association" do
     it { should belong_to(:category) }
@@ -13,30 +18,41 @@ RSpec.describe Video, type: :model do
   describe "validation" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:url)}
+    it { should validate_presence_of(:url) }
+    it { should validate_presence_of(:category_id) }
   end
 
   describe "creation" do
 
-    it "create a video" do
-      video = Video.create(title: "Basic rspec Ruby on Rails model specs",
-        description: "In this episode you'll learn some basics for testing simple methods on Ruby on Rails models.",
-        url: "https://www.youtube.com/watch?v=Spogv4o8haM",
-        category_id: category.id)
-      
+    it "create a video" do 
       expect(Video.count).to eq(1)
       expect(video.title).to eq("Basic rspec Ruby on Rails model specs")
     end
 
     it "find video by id" do
-      video = Video.create(title: "Basic rspec Ruby on Rails model specs",
-        description: "In this episode you'll learn some basics for testing simple methods on Ruby on Rails models.",
-        url: "https://www.youtube.com/watch?v=Spogv4o8haM",
-        category_id: category.id)
-      
       expect(video.id).to eq(1)
       expect(video.title).to eq("Basic rspec Ruby on Rails model specs")
     end
-    
+
+    it "update video by id" do
+      update_video = Video.find(video.id)
+      update_video.title = "New title for video"
+      
+      expect(update_video.id).to eq(1)
+      expect(update_video.title).to eq("New title for video")
+    end
+
+    it "does not update video by id when id is not found" do
+      update_video = Video.find_by(id: 2)
+      expect(update_video).to be_nil
+      
+      result = update_video&.update(title: "New title")
+      expect(result).to be_falsey
+    end
+
+    it "remove video by id" do
+      expect { Video.destroy(video.id) }.to change { Video.count }.by(-1)
+      expect { Video.find(video.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
